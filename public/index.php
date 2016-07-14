ddfd61d9deed1c3fe5b39caa0c39bb289890c584
David Moura <davidmoura@gmail.com>
2016-07-14 14:12:28 -0300

    Corrigindo autoload

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
-require_once "../autoload.php";
+define('CLASS_DIR', '../src/');
+set_include_path(get_include_path().PATH_SEPARATOR.CLASS_DIR);
+spl_autoload_register();
 
 use SON\Elements\Campo;
 use SON\Form;
